309162baa85ec4a9abdfcab4d5c2836aeb79848c
ThordurPall <ThordurPall@users.noreply.github.com>
2021-06-10 12:18:08 +0200

    Adding a comment

--- src/data/MakeDataset.py
+++ src/data/MakeDataset.py
@@ -31,3 +31,4 @@ class MakeDataset():
         #with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         #    zip_ref.extractall(directory_to_extract_to)
         #!unzip ./horse2zebra.zip > /dev/null
+        # TEST
