0904408a932c39734792245d0175f7724457f2c8
ThordurPall <ThordurPall@users.noreply.github.com>
2021-06-15 11:39:26 +0200

    Adding hyperparameters class and refactoring test

---
+++ src/models/Hyperparameters.py
@@ -0,0 +1,11 @@
+import json
+
+
+class Hyperparameters():
+    def __init__(self):
+        super().__init__()
+        f = open('./src/hyperparameters.json',)
+        self.config = json.load(f)
+        
+    def config(self):
+        return self.config
