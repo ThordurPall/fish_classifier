9a70b47c986aaedc3ae2eae06474ce31e14c3327
s161800 <s161800@student.dtu.dk>
2021-06-23 10:55:25 +0200

    adding files afgain

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     packages=find_packages(),
-    version="0.1.19",
+    version="0.1.31",
     description="A neural network fish classifier using a large-scale dataset for fish segmentation and classification",
     author="DTU",
     license="MIT",
